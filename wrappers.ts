8ea709dff992b7f975afcde4684647c55c1a07d1
Max Isom <hi@maxisom.me>
2022-01-25 16:56:55 -0500

    Fix typings

--- wrappers.ts
+++ wrappers.ts
@@ -1,3 +1,5 @@
+import type {NextApiRequest, NextApiResponse} from "next"
+
 /*
 
 Wraps a function in layers of other functions, while preserving the input/output
@@ -46,28 +48,34 @@ const withLoggedArguments =
 
 */
 
-// TODO middleware should have a required type
-type wrap1 = <T>(wf: T) => T
-type wrap2 = <T>(mw1: any, wf: T) => T
-type wrap3 = <T>(mw1: any, mw2: any, wf: T) => T
-type wrap4 = <T>(mw1: any, mw2: any, mw3: any, wf: T) => T
-type wrap5 = <T>(mw1: any, mw2: any, mw3: any, mw4: any, wf: T) => T
-type wrap6 = <T>(mw1: any, mw2: any, mw3: any, mw4: any, mw5: any, wf: T) => T
-type wrap7 = <T>(mw1: any, mw2: any, mw3: any, mw4: any, mw5: any, mw6: any, wf: T) => T
+type APIHandler<Request extends NextApiRequest> = (req: Request, res: NextApiResponse) => Promise<any>;
 
-type Wrappers = wrap1 & wrap2 & wrap3 & wrap4 & wrap5 & wrap6 & wrap7
+type Func<PrevousRequest extends NextApiRequest, NextRequest extends NextApiRequest> = (next: APIHandler<NextRequest>) => APIHandler<PrevousRequest>;
 
-export const wrappers: Wrappers = (...wrappersArgs) => {
-  const wrappedFunction = wrappersArgs[wrappersArgs.length - 1]
-  const mws = wrappersArgs.slice(0, -1)
+// type wrap1 = <T>(wf: T) => T
+// type wrap2 = <T>(mw1: Func, wf: T) => T
+// type wrap3 = <T>(mw1: Func, mw2: Func, wf: T) => T
+// type wrap4 = <T>(mw1: Func, mw2: Func, mw3: Func, wf: T) => T
+// type wrap5 = <T>(mw1: Func, mw2: Func, mw3: Func, mw4: Func, wf: T) => T
+// type wrap6 = <T>(mw1: Func, mw2: Func, mw3: Func, mw4: Func, mw5: Func, wf: T) => T
+// type wrap7 = <T>(mw1: Func, mw2: Func, mw3: Func, mw4: Func, mw5: Func, mw6: Func, wf: T) => T
 
-  let lastWrappedFunction = wrappedFunction
-  for (let i = mws.length - 1; i >= 0; i--) {
-    lastWrappedFunction = mws[i](lastWrappedFunction)
+type wrap2 = <Request extends NextApiRequest>(middlewares: [Func<NextApiRequest, Request>], handler: APIHandler<Request>) => APIHandler<Request>;
+
+type wrap3 = <Request extends NextApiRequest, Request2 extends Request>(middlewares: [
+  Func<NextApiRequest, Request>,
+  Func<Request, Request2>
+], handler: APIHandler<Request2>) => APIHandler<Request2>;
+
+type Wrappers = wrap2 | wrap3
+
+export const wrappers: wrap3 = (middlewares, handler) => {
+  let lastWrappedFunction = handler;
+  for (const middleware of middlewares) {
+    lastWrappedFunction = middleware(lastWrappedFunction as any)
   }
 
   return lastWrappedFunction
 }
 
-export default wrappers
-
+export default wrappers;
